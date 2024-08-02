832f88b2f6645db203f6308024e59131fe56ff9a
Jabari Crenshaw <36088613+jabaricrenshaw@users.noreply.github.com>
2024-08-02 12:10:59 -0700

    Code quality changes
    Added:
    Functional ticker for game loop instead of using "time.Sleep()" while waiting for frames
    Signal channel to exit game at any point, may be useful later

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
 module github.com/jabaricrenshaw/gameoflife
 
-go 1.22.3
+go 1.22.5
--- types/game.go
+++ types/game.go
@@ -1,14 +1,14 @@
 package gameoflife
 
 import (
-	"bufio"
 	"fmt"
 	"os"
+	"os/signal"
 	"time"
 )
 
-// Container type will be modified every frame
-// Options are set once on game init and only contain game "window size"
+// container     *Container 	will be modified every frame
+// Target_fps    float64 		should be exported so that it can be adjusted
 type Game struct {
 	container     *Container
 	frame_counter int
@@ -22,16 +22,13 @@ type Options struct {
 }
 
 func (gme *Game) init_cells() {
-	sc := bufio.NewScanner(os.Stdin)
-
 	fmt.Println(gme.container)
 	for {
 		var x_in, y_in int16 = -1, -1
 		fmt.Println("Cell activation.")
 		fmt.Printf("Provide X Y coordinates. (Within x=[0,%d] y=[0,%d]): ", gme.container.width-1, gme.container.height-1)
 
-		sc.Scan()
-		n, err := fmt.Sscanf(sc.Text(), "%d %d\n", &x_in, &y_in)
+		n, err := fmt.Scanf("%d %d\n", &x_in, &y_in)
 		if err == nil {
 			if (x_in >= 0 && x_in < gme.container.width) && (y_in >= 0 && y_in < gme.container.height) {
 				gme.container.grid[gme.container.height-y_in-1][x_in].active = !gme.container.grid[gme.container.height-y_in-1][x_in].active
@@ -39,7 +36,7 @@ func (gme *Game) init_cells() {
 			} else {
 				fmt.Println("Invalid coordinates!\n")
 			}
-		} else if n == 0 && err.Error() == "EOF" {
+		} else if n == 0 && err.Error() == "unexpected newline" {
 			break
 		} else {
 			fmt.Printf("%s\n\n", getInputErr(err))
@@ -48,43 +45,43 @@ func (gme *Game) init_cells() {
 }
 
 func get_opts() Options {
-	sc := bufio.NewScanner(os.Stdin)
-	var temp_w, temp_h int16
-
 	for {
-		temp_w, temp_h = -1, -1
-		fmt.Printf("Term size? (WxH): ")
+		var temp_w, temp_h int16 = -1, -1
+		fmt.Printf("Grid size? (WxH): ")
 
-		sc.Scan()
-		_, err := fmt.Sscanf(sc.Text(), "%d %d\n", &temp_w, &temp_h)
+		n, err := fmt.Scanf("%d %d\n", &temp_w, &temp_h)
 		if err != nil {
 			fmt.Printf("%s\n\n", getInputErr(err))
 		} else if temp_w <= 0 || temp_h <= 0 {
 			fmt.Println("Invalid size!\n")
-		} else {
-			fmt.Println("")
-			break
+		} else if n == 2 {
+			return Options{
+				width:  temp_w,
+				height: temp_h,
+			}
 		}
-	}
-
-	return Options{
-		width:  temp_w,
-		height: temp_h,
 	}
 }
 
 func getInputErr(err error) string {
 	switch err.Error() {
-	case "EOF":
+	case "unexpected newline":
 		return "Not enough values!"
 	case "newline in format does not match input":
 		return "Too many values!"
-	default:
-		return "Bad input."
 	}
+	return "Bad input."
 }
 
 func (gme *Game) Start() {
+	c := make(chan os.Signal, 1)
+	defer close(c)
+	signal.Notify(c, os.Interrupt)
+	go func() {
+		<-c
+		os.Exit(0)
+	}()
+
 	windowOpts := get_opts()
 	gme = &Game{
 		container:     NewContainer(windowOpts.width, windowOpts.height),
@@ -92,23 +89,23 @@ func (gme *Game) Start() {
 		frame_counter: 0,
 	}
 
+	// Setting up game loop timer
+	frame_wait_time := time.Second / time.Duration(gme.Target_fps)
+	t := time.NewTicker(frame_wait_time)
+	defer t.Stop()
+
+	// Getting user input to activate cells
 	gme.init_cells()
 
-	wait_time_ms := time.Second / time.Duration(gme.Target_fps)
-	var total_time time.Duration
-	for {
-		// Incrementing frame counter and showing stats
+	// Main game loop
+	for range t.C {
+		// Incrementing counter and showing stats
 		gme.frame_counter++
-		fmt.Printf("Frames elapsed: %d (%.2f FPS @ %s/frame)\n%s", gme.frame_counter, gme.Target_fps, wait_time_ms, gme.container)
+		fmt.Printf("Frames elapsed: %d (%.2f FPS @ %s/frame)\n%s", gme.frame_counter, gme.Target_fps, frame_wait_time, gme.container)
 
 		// Timing this frame update
-		start := time.Now()
+		start_frame_gen := time.Now()
 		gme.container.update()
-		fmt.Printf("(frame gen took: %s)\n\n", time.Since(start))
-
-		// Waiting for next frame
-
-		time.Sleep(wait_time_ms)
-		total_time += (time.Since(start))
+		fmt.Printf("(frame gen took: %s)\n\n", time.Since(start_frame_gen))
 	}
 }
